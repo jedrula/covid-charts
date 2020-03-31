fd589b18f26b7a9696fe767cc0007893b559ee42
Andrzej Swaton <andrzej@diligen.com>
2020-03-31 14:12:54 +0200

    Provide information about data source

--- src/components/CovidPage.vue
+++ src/components/CovidPage.vue
@@ -14,6 +14,7 @@
       :confirmedRows="selectedIndexes.map((selectedIndex) => covidConfirmedJson[selectedIndex])"
       :rowToCountry="rowToCountry"
     />
+    <footer>Data taken from <a target="_blank" href="https://github.com/CSSEGISandData/COVID-19">JHU CSSE</a></footer>
   </div>
 </template>
 
@@ -68,3 +69,9 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+footer {
+  margin-top: 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ const routes = [
 
 const router = new VueRouter({
   routes,
-  mode: 'history',
+  // mode: 'history',
 })
 
 new Vue({
